f729ea80dd7bba7dd130a9c3913e7cb55f92d6f9
minzion0 <aert12435@gmail.com>
2023-07-21 12:49:39 +0900

    Initial commit

---
+++ src/test/java/com/green/smart_grade/SmartGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.smart_grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
