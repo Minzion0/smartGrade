11c25a3ca63d514b7c44076f01ec343a7275776e
이민용 <aert12435@gmail.com>
2023-07-23 13:06:26 +0900

    PagingUtils 설정

--- src/main/java/com/green/smart_grade/utils/PagingUtils.java
+++ src/main/java/com/green/smart_grade/utils/PagingUtils.java
@@ -1,13 +1,15 @@
 package com.green.smart_grade.utils;
 
 public class PagingUtils {
+    private final static int ROW = 10;
+
     public static int startIdx(int page){
         int result = page - 1;
-        return  result*10;
+        return  result*ROW;
     }
 
     public static int maxPage(int maxPage){
-        return(int) Math.ceil((double) maxPage / 10);
+        return(int) Math.ceil((double) maxPage / ROW);
     }
 
     public static int isMore(int maxPage,int page){
