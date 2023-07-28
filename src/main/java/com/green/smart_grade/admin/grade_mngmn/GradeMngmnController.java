ff89c279d4533c3e5fc9660106fd6d61f1621493
진혁재 <jhj02201@gmail.com>
2023-07-28 14:52:30 +0900

    관리자 학생 성적관리 완료

--- src/main/java/com/green/smart_grade/admin/grade_mngmn/GradeMngmnController.java
+++ src/main/java/com/green/smart_grade/admin/grade_mngmn/GradeMngmnController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/grademngmn")
+@RequestMapping("/api/grade")
 @RequiredArgsConstructor
 @Tag(name = "통합 성적관리")
 public class GradeMngmnController {
