4bd7e0537e399bfc8d05e1729bedecd5c1273e50
진혁재 <jhj02201@gmail.com>
2023-08-09 16:22:52 +0900

    테스트 비활성화

--- src/test/java/com/green/smartGrade/admin/lectureroom/AdminLectureRoomMapperTest.java
+++ src/test/java/com/green/smartGrade/admin/lectureroom/AdminLectureRoomMapperTest.java
@@ -1,31 +1,31 @@
-package com.green.smartGrade.admin.lectureroom;
-
-import com.green.smartGrade.admin.lectureroom.model.LectureRoomInsDto;
-import org.junit.jupiter.api.Test;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@MybatisTest
-@ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class AdminLectureRoomMapperTest {
-
-    @Autowired
-    private AdminLectureRoomMapper mapper;
-
-    @Test
-    void insLectureRoom() {
-    }
-
-    @Test
-    void selLectureRoom() {
-    }
-
-    @Test
-    void delLectureRoom() {
-    }
-}
+//package com.green.smartGrade.admin.lectureroom;
+//
+//import com.green.smartGrade.admin.lectureroom.model.LectureRoomInsDto;
+//import org.junit.jupiter.api.Test;
+//import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@MybatisTest
+//@ActiveProfiles("test")
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//class AdminLectureRoomMapperTest {
+//
+//    @Autowired
+//    private AdminLectureRoomMapper mapper;
+//
+//    @Test
+//    void insLectureRoom() {
+//    }
+//
+//    @Test
+//    void selLectureRoom() {
+//    }
+//
+//    @Test
+//    void delLectureRoom() {
+//    }
+//}
