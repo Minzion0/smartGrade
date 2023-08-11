892393a92885b3e4ce0ff26725fb74ca8c1fc106
김재경 <worud4227@naver.com>
2023-08-11 09:42:44 +0900

    boardTestMapper

---
+++ src/test/java/com/green/smartGrade/admin/board/BoardMapperTest.java
@@ -0,0 +1,53 @@
+package com.green.smartGrade.admin.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class BoardMapperTest {
+
+    @Test
+    void insBoard() {
+    }
+
+    @Test
+    void insBoardPic() {
+    }
+
+    @Test
+    void updBoard() {
+    }
+
+    @Test
+    void selBoard() {
+    }
+
+    @Test
+    void selSearchBoard() {
+    }
+
+    @Test
+    void countSearchBoard() {
+    }
+
+    @Test
+    void selBoardImportance() {
+    }
+
+    @Test
+    void countBoard() {
+    }
+
+    @Test
+    void selByIdBoard() {
+    }
+
+    @Test
+    void updDelYnBoard() {
+    }
+
+    @Test
+    void updViewBoard() {
+    }
+}
