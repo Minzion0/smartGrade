52269be41381644c183f0c90e0b1ddd7d0343970
minzion0 <aert12435@gmail.com>
2023-08-12 12:11:30 +0900

    swagger설정

--- src/main/java/com/green/smartGrade/config/exception/SmartGradeExceptionHandler.java
+++ src/main/java/com/green/smartGrade/config/exception/SmartGradeExceptionHandler.java
@@ -20,12 +20,12 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
 
-
+    //401ExceptionHandler
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Object> handleClientErrorException(HttpServletRequest request){
         return handleExceptionInternal(CommonErrorCode.AUTHENTICATION_ERROR,CommonErrorCode.AUTHENTICATION_ERROR.getMessage(),request.getRequestURI());
     }
-
+    //admin영역 ExceptionHandler
     @ExceptionHandler(AdminException.class)
     public ResponseEntity<Object> handleAdminException(AdminException e, HttpServletRequest request){
         return handleAdminExceptionSet(CommonErrorCode.ADMIN_EXCEPTION, e.getMsg(),request.getRequestURI());
@@ -33,14 +33,13 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
 
 
 
-    // IllegalArgumentException 에러 처리
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e,HttpServletRequest request) {
         log.warn("handleIllegalArgument", e);
         return handleExceptionInternal(CommonErrorCode.GLOBAL_EXCEPTION, e.getMessage(),request.getRequestURI());
     }
 
-    // 대부분의 에러 처리
+    //설정한것 이외의 모든 에러 처리
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleAllException(Exception ex,HttpServletRequest request) {
         log.warn("handleAllException", ex);
@@ -51,7 +50,7 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(CommonErrorCode.GLOBAL_EXCEPTION,CommonErrorCode.GLOBAL_EXCEPTION.getMessage(),request.getRequestURI());
     }
 
-    // RuntimeException과 대부분의 에러 처리 메세지를 보내기 위한 메소드
+
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(makeErrorResponse(errorCode));
@@ -61,7 +60,7 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(errorCode.getHttpStatus()).body(makeErrorResponse(errorCode,msg,path));
     }
 
-    // 코드 가독성을 위해 에러 처리 메세지를 만드는 메소드 분리
+    // 에러 메시지 정리
     private MyErrorResponse makeErrorResponse(ErrorCode errorCode) {
         return MyErrorResponse.builder()
                 .code(errorCode.name())
@@ -81,7 +80,7 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
                 .message(message)
                 .build();
     }
-    // 코드 가독성을 위해 에러 처리 메세지를 만드는 메소드 분리
+    // 에러 메시지 정리
     private MyErrorResponse makeErrorResponse(ErrorCode errorCode, String message,String path) {
         return MyErrorResponse.builder()
                 .code(errorCode.getMessage())
@@ -97,7 +96,7 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(makeErrorResponse(e, errorCode));
     }
 
-    // 코드 가독성을 위해 에러 처리 메세지를 만드는 메소드 분리
+    // 에러 메시지 정리
     private MyErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
         List<MyErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
                 .getFieldErrors()
@@ -112,7 +111,7 @@ public class SmartGradeExceptionHandler extends ResponseEntityExceptionHandler {
                 .errors(validationErrorList)
                 .build();
     }
-
+    //에러 발생시간
     private String now(){
         LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-dd-MM HH:mm:ss"));
         SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd // HH:mm:ss z");
