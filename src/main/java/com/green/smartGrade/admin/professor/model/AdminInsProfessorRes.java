695a494a52aca7d46fd0b6d32334b1eaaefac346
minzion0 <aert12435@gmail.com>
2023-08-14 10:10:13 +0900

    코드 정리

--- src/main/java/com/green/smartGrade/admin/professor/model/AdminInsProfessorRes.java
+++ src/main/java/com/green/smartGrade/admin/professor/model/AdminInsProfessorRes.java
@@ -16,19 +16,7 @@ public class AdminInsProfessorRes {
     private LocalDateTime createdAt;
     private int delYn;
 
-//    public AdminInsProfessorRes(AdminInsProfessorDto dto) {
-//        this.iprofessor = dto.getIprofessor();
-//        this.imajor = dto.getImajor();
-//
-//        this.nm = dto.getNm();
-//        this.gender = dto.getGender();
-//        this.birthdate = dto.getBirthdate();
-//        this.phone = dto.getPhone();
-//        this.email = dto.getEmail();
-//        this.address = dto.getAddress();
-//        this.createdAt = LocalDateTime.now();
-//        this.delYn = 0;
-//    }
+
 
     public void setRes(AdminInsProfessorDto dto){
         this.iprofessor = dto.getIprofessor();
