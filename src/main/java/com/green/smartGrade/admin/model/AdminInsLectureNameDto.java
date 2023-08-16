85d46eaf0a12a416e02ccaefd0e0cc352fbfe10a
minzion0 <aert12435@gmail.com>
2023-08-16 13:14:50 +0900

    강의명 등록 조회 추가

---
+++ src/main/java/com/green/smartGrade/admin/model/AdminInsLectureNameDto.java
@@ -0,0 +1,10 @@
+package com.green.smartGrade.admin.model;
+
+import lombok.Data;
+
+@Data
+public class AdminInsLectureNameDto {
+    private Long ilectureName;
+    private String lectureName;
+    private  int score;
+}
