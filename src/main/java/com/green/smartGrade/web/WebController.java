1f0b00a3cdb166d331599bc3e1029ca664f5e17a
김재경 <worud4227@naver.com>
2023-08-17 18:12:06 +0900

    build jk

--- src/main/java/com/green/smartGrade/web/WebController.java
+++ src/main/java/com/green/smartGrade/web/WebController.java
@@ -4,13 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Controller
-public class WebController implements ErrorController {
-    @GetMapping({"/", "/error"})
-    public String index() {
-        log.info("------------------------asdf----------------------");
-        return "index.html";
-    }
+public class WebController implements WebMvcConfigurer {
+//    @GetMapping({"/", "/error"})
+//    public String index() {
+//        log.info("------------------------asdf----------------------");
+//        return "index.html";
+//    }
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("{spring:\\w+}").setViewName("forward:/");
+//        registry.addViewController("/**{spring:\\w+}").setViewName("forward:/");
+//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
+//    }
 }
